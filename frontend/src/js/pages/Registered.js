9f066a4c8517e54d1979aaf883070d6be2e41bab
Quentin Chevrin <qchevrin@pictet.com>
2019-12-18 15:56:11 +0100

    logo

--- frontend/src/js/pages/Registered.js
+++ frontend/src/js/pages/Registered.js
@@ -1,24 +1,23 @@
 import React from 'react'
+import logoNoBorder from '../../assets/logo-no-border.png'
 
 const Checkmark = () => (
-  <svg
-    class="Registered--checkmark"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 52 52"
-  >
-    <circle
-      class="Registered--checkmarkCircle"
-      cx="26"
-      cy="26"
-      r="25"
-      fill="none"
-    />
-    <path
-      class="Registered--checkmarkCheck"
-      fill="none"
-      d="M14.1 27.2l7.1 7.2 16.7-16.8"
-    />
-  </svg>
+  <div className="Registered--checkmarkContainer">
+    <svg
+      class="Registered--checkmark"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 52 52"
+    >
+      <circle
+        class="Registered--checkmarkCircle"
+        cx="26"
+        cy="26"
+        r="25"
+        fill="none"
+      />
+    </svg>
+    <img alt="Logo" src={logoNoBorder} className="Registered--names" />
+  </div>
 )
 
 const Registered = () => {
