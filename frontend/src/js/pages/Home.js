439967db640a3012d79fcdf8f15f990a98aad83c
Quentin Chevrin <qchevrin@pictet.com>
2020-01-09 16:29:08 +0100

    call to action form

--- frontend/src/js/pages/Home.js
+++ frontend/src/js/pages/Home.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { Link } from 'react-router-dom'
+
+const HomeEmpty = () => {
+  return (
+    <div className="HomeEmpty">
+      <p>
+        Tu viens ? Avec qui ?<br />
+        Aide nous à organiser ce beau week-end ensemble
+      </p>
+      <Link to="/guest" className="Button">
+        Je viens !
+      </Link>
+    </div>
+  )
+}
 
 const Home = () => {
-  return <Layout></Layout>
+  return (
+    <Layout>
+      <HomeEmpty />
+    </Layout>
+  )
 }
 
 export default Home
