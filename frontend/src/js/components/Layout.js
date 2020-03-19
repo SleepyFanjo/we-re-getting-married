1995b8373ab7c596c647f7295a8a6b1a16e742c9
Quentin Chevrin <qchevrin@pictet.com>
2020-03-19 10:57:53 +0100

    update graphqiue

--- frontend/src/js/components/Layout.js
+++ frontend/src/js/components/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from '../../assets/logo.png'
+import logo from '../../assets/logo.svg'
 import { Link, useLocation } from 'react-router-dom'
 
 import routes from '../routes'
--- frontend/src/js/pages/Register.js
+++ frontend/src/js/pages/Register.js
@@ -1,21 +1,23 @@
 import React from 'react'
+import Layout from '../components/Layout'
 import EmailRegister from '../components/EmailRegister'
 
 const Register = () => {
   return (
-    <div className="Register--container">
-      <p>
-        Hello, on sera super content de te voir en Septembre. Laisse nous une
-        adresse email pour confirmer ta venue, elle nous sera utile pour te
-        transmettre toutes les infos.
-      </p>
-      <EmailRegister />
-      <p>
-        Tu peux aussi nous confirmer ta venue par téléphone au XX.XX.XX.XX.XX
-        <br />
-        Ou par email à mariage@gmail.com
-      </p>
-    </div>
+    <Layout noFooter>
+      <div className="Register--container">
+        <p>
+          Ah ! En voila une bonne nouvelle !<br /> <br />
+          Laisse nous ton nom et ton email pour que nous puissions te créer un
+          compte sur le site.
+          <br />
+          Il te permettra de nous confirmer ta venue ainsi que celle de ton
+          entourage, réserver ta chambre au château et nous indiquer si tu as un
+          régime particulier.
+        </p>
+        <EmailRegister />
+      </div>
+    </Layout>
   )
 }
 
